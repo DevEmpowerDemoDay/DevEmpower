4425051ce6e1138ccd809d6116425bcff71df45a
fatekkl <mathtml.1105@gmail.com>
2024-10-16 15:07:17 -0300

    fix

--- app/src/main/java/com/demoday/devempower/MainActivity.kt
+++ app/src/main/java/com/demoday/devempower/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             DevEmpowerTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Matheus",
+                        name = "DevEmpower",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
