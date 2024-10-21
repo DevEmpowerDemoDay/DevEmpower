0b4a16510c220d41accb68d2b65de01ea8d1e9de
fatekkl <mathtml.1105@gmail.com>
2024-10-21 17:36:59 -0300

    Criando exemplo de NavigationMenu

--- app/src/main/java/com/demoday/devempower/MainActivity.kt
+++ app/src/main/java/com/demoday/devempower/MainActivity.kt
@@ -4,13 +4,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.demoday.devempower.ui.theme.DevEmpowerTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,12 +29,8 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             DevEmpowerTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "CurtisWhite",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                Scaffold(modifier = Modifier.fillMaxSize(), bottomBar = { BottomNavigationMenu() }) { innerPadding ->
+                    Greeting(name = "Matheus", Modifier.padding(innerPadding))                }
             }
         }
     }
@@ -36,6 +42,23 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
         text = "Hello $name!",
         modifier = modifier
     )
+}
+
+@Composable
+fun BottomNavigationMenu() {
+
+    Box(modifier = Modifier.fillMaxSize()) {
+        NavigationBar(
+            containerColor = Color.Gray,
+            modifier = Modifier
+                .align(Alignment.BottomCenter)
+                .size(width = 357.dp, height = 66.dp)
+                .offset(y = (-66).dp)
+                .clip(RoundedCornerShape(38.dp))
+        ) {
+
+        }
+    }
 }
 
 @Preview(showBackground = true)
