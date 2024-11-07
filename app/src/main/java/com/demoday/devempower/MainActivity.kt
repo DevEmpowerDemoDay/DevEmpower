cc0119661b32aa05d0a6188c15e8e414c6feb6fe
fatekkl <mathtml.1105@gmail.com>
2024-11-07 12:19:34 -0300

    finish merge

--- app/src/main/java/com/demoday/devempower/MainActivity.kt
+++ app/src/main/java/com/demoday/devempower/MainActivity.kt
@@ -28,67 +28,16 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
-import androidx.compose.ui.text.font.Font
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import com.demoday.devempower.ui.theme.DevEmpowerTheme
-
-<<<<<<< HEAD
-import androidx.compose.foundation.background
-<<<<<<< HEAD
-=======
-=======
->>>>>>> dev
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Row
->>>>>>> dev
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.offset
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Home
-import androidx.compose.material.icons.filled.Person
-import androidx.compose.material3.Icon
-import androidx.compose.material3.NavigationBar
-import androidx.compose.material3.NavigationBarItem
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.colorResource
-import androidx.compose.ui.text.font.Font
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.demoday.devempower.ui.theme.DevEmpowerTheme
 
 class MainActivity : ComponentActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             DevEmpowerTheme {
-<<<<<<< HEAD
-                val primaryColor = colorResource(id = R.color.celadon)
-                Scaffold(modifier = Modifier.fillMaxSize(), containerColor = primaryColor) { innerPadding ->
-                    Greeting(
-                        name = "CurtisTeste",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-=======
                 val uranium_blue = colorResource(id = R.color.uranium_blue)
                 val celadon = colorResource(id = R.color.celadon)
                 val davys_gray = colorResource(id = R.color.davys_gray)
@@ -100,7 +49,6 @@ class MainActivity : ComponentActivity() {
                     containerColor = uranium_blue,
                     bottomBar = { BottomNavigationMenu() }) { innerPadding ->
                     Greeting(name = "Matheus", Modifier.padding(innerPadding))
->>>>>>> dev
                 }
             }
         }
@@ -109,15 +57,9 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-
-    val customFontFamily = FontFamily(
-        Font(R.font.poppins)  // Aqui, o nome do arquivo da fonte
-    )
-
     Text(
         text = "Hello $name!",
-        modifier = modifier,
-        fontFamily = customFontFamily,
+        modifier = modifier
     )
 }
 
@@ -185,5 +127,5 @@ fun BottomNavigationMenu() {
 fun GreetingPreview() {
     DevEmpowerTheme {
         Greeting("Android")
-        }
+    }
 }
