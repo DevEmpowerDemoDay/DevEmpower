e985793abee4b0393cbafb660c24ad842d7d742f
fatekkl <mathtml.1105@gmail.com>
2024-11-07 12:13:54 -0300

    update imports

--- app/src/main/java/com/demoday/devempower/MainActivity.kt
+++ app/src/main/java/com/demoday/devempower/MainActivity.kt
@@ -4,11 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
