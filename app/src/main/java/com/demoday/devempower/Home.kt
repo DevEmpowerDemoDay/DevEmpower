a8be53e53113d8384f7a8c387953e325601887a1
MuriloCoelho01 <coelho.murilo10@gmail.com>
2024-11-07 18:20:14 -0300

    Tela Splash

---
+++ app/src/main/java/com/demoday/devempower/Home.kt
@@ -0,0 +1,92 @@
+package com.demoday.devempower
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+import com.demoday.devempower.ui.theme.DevEmpowerTheme
+
+@Composable
+fun BottomNavigationMenu(navController: NavController) {
+    val uranium_blue = colorResource(id = R.color.uranium_blue)
+    val celadon = colorResource(id = R.color.celadon)
+    val davys_gray = colorResource(id = R.color.davys_gray)
+    val white_smoke = colorResource(id = R.color.white_smoke)
+    val indigo_dye = colorResource(id = R.color.indigo_dye)
+
+    val selectedIndex = remember { mutableStateOf(0) } // Para rastrear qual botão está selecionado
+
+    Box(modifier = Modifier.fillMaxSize()) {
+        NavigationBar(
+            containerColor = indigo_dye,
+            modifier = Modifier
+                .align(Alignment.BottomCenter)
+                .size(width = 357.dp, height = 176.dp)
+                .offset(y = (-66).dp)
+                .clip(RoundedCornerShape(38.dp))
+        ) {
+            Row {
+                NavigationBarItem(
+                    selected = selectedIndex.value == 0,
+                    onClick = { selectedIndex.value = 0 },
+                    icon = {
+                        Icon(
+                            imageVector = Icons.Default.Home,
+                            contentDescription = "Home",
+                            modifier = Modifier
+                                .size(width = 26.dp, height = 26.dp)
+                                .clip(CircleShape),
+                            tint = Color.Black,
+                        )
+                    },
+                    modifier = Modifier.weight(1f) // Adicione esta linha
+                )
+
+                NavigationBarItem(
+                    selected = selectedIndex.value == 0,
+                    onClick = { selectedIndex.value = 0 },
+                    icon = {
+                        Icon(
+                            imageVector = Icons.Default.Person,
+                            contentDescription = "Home",
+                            modifier = Modifier
+                                .size(width = 26.dp, height = 46.dp)
+                                .clip(CircleShape),
+                            tint = Color.Black,
+                        )
+                    },
+                )
+            }
+
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun BottonPreview() {
+    DevEmpowerTheme {
+        BottomNavigationMenu(rememberNavController())
+    }
+}
--- app/src/main/java/com/demoday/devempower/MainActivity.kt
+++ app/src/main/java/com/demoday/devempower/MainActivity.kt
@@ -4,128 +4,112 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Home
-import androidx.compose.material.icons.filled.Person
-import androidx.compose.material3.Icon
-import androidx.compose.material3.NavigationBar
-import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.demoday.devempower.ui.theme.DevEmpowerTheme
 
+
 class MainActivity : ComponentActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             DevEmpowerTheme {
+                Inicio()
+            }}}}
+
+
+            @Composable
+fun Inicio() {
                 val uranium_blue = colorResource(id = R.color.uranium_blue)
                 val celadon = colorResource(id = R.color.celadon)
                 val davys_gray = colorResource(id = R.color.davys_gray)
                 val white_smoke = colorResource(id = R.color.white_smoke)
                 val indigo_dye = colorResource(id = R.color.indigo_dye)
+                val navController = rememberNavController()
 
                 Scaffold(
                     modifier = Modifier.fillMaxSize(),
-                    containerColor = uranium_blue,
-                    bottomBar = { BottomNavigationMenu() }) { innerPadding ->
-                    Greeting(name = "Matheus", Modifier.padding(innerPadding))
-                }
+                    content = { innerPadding ->
+                        Column(
+                            modifier = Modifier
+                                .padding(innerPadding)
+                                .fillMaxSize(),
+                            verticalArrangement = Arrangement.Center,
+                            horizontalAlignment = Alignment.CenterHorizontally
+                        ){
+
+                            NavHost(navController = navController, startDestination = "login") {
+                                composable("login") { Splash(navController) }
+                                composable("login") { BottomNavigationMenu(navController) }
+
+
+
+                            }
+                        }
+                    },
+
+                    )
             }
-        }
-    }
-}
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-
-
-@Composable
-fun BottomNavigationMenu() {
+fun Splash(navController: NavController) {
     val uranium_blue = colorResource(id = R.color.uranium_blue)
     val celadon = colorResource(id = R.color.celadon)
     val davys_gray = colorResource(id = R.color.davys_gray)
     val white_smoke = colorResource(id = R.color.white_smoke)
     val indigo_dye = colorResource(id = R.color.indigo_dye)
 
-    val selectedIndex = remember { mutableStateOf(0) } // Para rastrear qual botão está selecionado
+Column(
+    modifier = Modifier
+    .fillMaxSize()
+        .background(color = uranium_blue),
+    verticalArrangement = Arrangement.Center,
+    horizontalAlignment = Alignment.CenterHorizontally
+)
+{
+    Image(
+        painter = painterResource(R.drawable.vector),
+        contentDescription = ""
+    )
+    Text(text ="DevEmpower",
+        fontSize = 32.sp,
+        color = Color(0xFFFF005377))
 
-    Box(modifier = Modifier.fillMaxSize()) {
-        NavigationBar(
-            containerColor = indigo_dye,
-            modifier = Modifier
-                .align(Alignment.BottomCenter)
-                .size(width = 357.dp, height = 176.dp)
-                .offset(y = (-66).dp)
-                .clip(RoundedCornerShape(38.dp))
-        ) {
-            Row {
-                NavigationBarItem(
-                    selected = selectedIndex.value == 0,
-                    onClick = { selectedIndex.value = 0 },
-                    icon = {
-                        Icon(
-                            imageVector = Icons.Default.Home,
-                            contentDescription = "Home",
-                            modifier = Modifier
-                                .size(width = 26.dp, height = 26.dp)
-                                .clip(CircleShape),
-                            tint = Color.Black,
-                        )
-                    },
-                    modifier = Modifier.weight(1f) // Adicione esta linha
-                )
-
-                NavigationBarItem(
-                    selected = selectedIndex.value == 0,
-                    onClick = { selectedIndex.value = 0 },
-                    icon = {
-                        Icon(
-                            imageVector = Icons.Default.Person,
-                            contentDescription = "Home",
-                            modifier = Modifier
-                                .size(width = 26.dp, height = 46.dp)
-                                .clip(CircleShape),
-                            tint = Color.Black,
-                        )
-                    },
-                )
-            }
-
-        }
-    }
+    Image(
+        painter = painterResource(R.drawable.logotipo),
+contentDescription = ""    )
+}
 }
 
 
-@Preview(showBackground = true)
+
+@Preview
 @Composable
-fun GreetingPreview() {
+private fun preview() {
     DevEmpowerTheme {
-        Greeting("Android")
-        }
+        Splash(rememberNavController())
+    }
 }
