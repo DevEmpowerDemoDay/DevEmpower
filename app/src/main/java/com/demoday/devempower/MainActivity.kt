45150fceac34a514b67a765799bc74e66b07f2eb
fatekkl <mathtml.1105@gmail.com>
2024-11-07 11:56:52 -0300

    import font + colors

--- app/src/main/java/com/demoday/devempower/MainActivity.kt
+++ app/src/main/java/com/demoday/devempower/MainActivity.kt
@@ -4,24 +4,32 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import com.demoday.devempower.ui.theme.DevEmpowerTheme
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             DevEmpowerTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                val primaryColor = colorResource(id = R.color.celadon)
+                Scaffold(modifier = Modifier.fillMaxSize(), containerColor = primaryColor) { innerPadding ->
                     Greeting(
-                        name = "CurtisWhite",
+                        name = "CurtisTeste",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -32,9 +40,15 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
+
+    val customFontFamily = FontFamily(
+        Font(R.font.poppins)  // Aqui, o nome do arquivo da fonte
+    )
+
     Text(
         text = "Hello $name!",
-        modifier = modifier
+        modifier = modifier,
+        fontFamily = customFontFamily,
     )
 }
 
