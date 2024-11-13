b3ec64677b742e2fe5065f4ca3e4625b4c34b9d3
Maykon Silva <140466040+maykon3@users.noreply.github.com>
2024-11-12 22:51:36 -0300

    Telas de redefinir senha / Senha alterada

--- app/src/main/java/com/demoday/devempower/RedefinirSenha.kt
+++ app/src/main/java/com/demoday/devempower/RedefinirSenha.kt
@@ -20,6 +20,7 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -43,6 +44,7 @@ import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import com.demoday.devempower.ui.theme.DevEmpowerTheme
+import kotlinx.coroutines.delay
 
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -67,7 +69,7 @@ fun RedefinirSenha(navController: NavController) {
             fontSize = 20.sp,
             fontWeight = FontWeight.SemiBold,
             color = midnight_blue,
-            fontFamily = fontPoppins,
+            //fontFamily = fontPoppins,
             modifier = Modifier
                 .padding(top = 230.dp)
         )
@@ -87,13 +89,13 @@ fun RedefinirSenha(navController: NavController) {
                     "Esqueceu sua senha?",
                     fontSize = 13.sp,
                     color = bright_blue,
-                    fontFamily = fontPoppins,
+                    //fontFamily = fontPoppins,
                 )
                 Spacer(modifier = Modifier.padding(start = 5.dp))
                 Text(
                     "Sem problemas!",
                     fontSize = 13.sp,
-                    fontFamily = fontPoppins,
+                    //fontFamily = fontPoppins,
                     color = midnight_blue
                 )
             }
@@ -104,7 +106,7 @@ fun RedefinirSenha(navController: NavController) {
                 Text(
                     " Vamos ajudá-lo a criar uma nova",
                     fontSize = 13.sp,
-                    fontFamily = fontPoppins,
+                    //fontFamily = fontPoppins,
                     color = midnight_blue
                 )
             }
@@ -116,7 +118,7 @@ fun RedefinirSenha(navController: NavController) {
             Text(
                 "Por favor, insira o endereço de e-mail",
                 fontSize = 13.sp,
-                fontFamily = fontPoppins,
+                //fontFamily = fontPoppins,
                 fontWeight = FontWeight.SemiBold,
                 color = midnight_blue
             )
@@ -125,7 +127,7 @@ fun RedefinirSenha(navController: NavController) {
             Text(
                 "associado à sua conta. Enviaremos um",
                 fontSize = 13.sp,
-                fontFamily = fontPoppins,
+                //fontFamily = fontPoppins,
                 fontWeight = FontWeight.SemiBold,
                 color = midnight_blue
             )
@@ -134,7 +136,7 @@ fun RedefinirSenha(navController: NavController) {
             Text(
                 "link para redefinir sua senha.",
                 fontSize = 13.sp,
-                fontFamily = fontPoppins,
+                //fontFamily = fontPoppins,
                 fontWeight = FontWeight.SemiBold,
                 color = midnight_blue
             )
@@ -154,7 +156,7 @@ fun RedefinirSenha(navController: NavController) {
                     "Email",
                     fontSize = 12.sp,
                     fontWeight = FontWeight.Normal,
-                    fontFamily = fontPoppins,
+                    //fontFamily = fontPoppins,
                 )
             },
             modifier = Modifier
@@ -177,7 +179,7 @@ fun RedefinirSenha(navController: NavController) {
         Spacer(modifier = Modifier.padding(top = 30.dp))
 
         Button(
-            onClick = { /* ação ao clicar */ },
+            onClick = { navController.navigate("mudarsenha") },
             colors = ButtonDefaults.buttonColors(
                 containerColor = indigo_dye
             ),
@@ -190,7 +192,7 @@ fun RedefinirSenha(navController: NavController) {
                 text = "Enviar",
                 color = white_smoke,
                 fontSize = 20.sp,
-                fontFamily = fontPoppins,
+                //fontFamily = fontPoppins,
                 modifier = Modifier
                     .align(alignment = Alignment.CenterVertically)
 
@@ -200,11 +202,185 @@ fun RedefinirSenha(navController: NavController) {
     }
 }
 
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MudarSenha(navController: NavController) {
+
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .fillMaxSize()
+            .paint(
+                painter = painterResource(
+                    R.drawable.background_color
+                ),
+                contentScale = ContentScale.Crop
+            )
+    ) {
+        Text(
+            "Redefinir a senha",
+            fontSize = 20.sp,
+            fontWeight = FontWeight.SemiBold,
+            color = midnight_blue,
+            //fontFamily = fontPoppins,
+            modifier = Modifier
+                .padding(top = 230.dp)
+        )
+
+        Image(
+            painter = painterResource(R.drawable.blue_line),
+            contentDescription = "linha az",
+            modifier = Modifier
+                .size(width = 163.dp, height = 4.dp)
+        )
+
+        Spacer(modifier = Modifier.padding(top = 25.dp))
+
+        var senha by remember {
+            mutableStateOf("")
+        }
+
+        Row {
+            Text(
+                "Nova Senha:",
+                fontSize = 18.sp,
+                fontWeight = FontWeight.Normal,
+                //fontFamily = fontPoppins,
+            )
+        }
+
+        Spacer(modifier = Modifier.padding(top = 10.dp))
+
+        androidx.compose.material3.OutlinedTextField(
+            value = senha,
+            onValueChange = { novoTexto -> senha = novoTexto },
+            modifier = Modifier
+                .width(274.43.dp)
+                .height(45.dp),
+            shape = RoundedCornerShape(7.dp),
+            colors = TextFieldDefaults.textFieldColors(
+                containerColor = Color(0xFFF3F3F3)
+            ),
+            textStyle = TextStyle(
+                fontSize = 18.sp,
+                textAlign = TextAlign.Start
+            ),
+            singleLine = true
+        )
+
+        Spacer(modifier = Modifier.padding(10.dp))
+
+        var confirma by remember {
+            mutableStateOf("")
+        }
+
+        Row {
+            Text(
+                "Nova Senha:",
+                fontSize = 18.sp,
+                fontWeight = FontWeight.Normal,
+                //fontFamily = fontPoppins,
+            )
+        }
+
+        Spacer(modifier = Modifier.padding(top = 10.dp))
+
+        androidx.compose.material3.OutlinedTextField(
+            value = confirma,
+            onValueChange = { novoTexto -> confirma = novoTexto },
+            modifier = Modifier
+                .width(274.43.dp)
+                .height(45.dp),
+            shape = RoundedCornerShape(7.dp),
+            colors = TextFieldDefaults.textFieldColors(
+                containerColor = Color(0xFFF3F3F3)
+            ),
+            textStyle = TextStyle(
+                fontSize = 18.sp,
+                textAlign = TextAlign.Start
+            ),
+            singleLine = true
+        )
+
+        Spacer(modifier = Modifier.padding(top = 20.dp))
+
+
+        Button(
+            onClick = { navController.navigate("senhaalterada") },
+            colors = ButtonDefaults.buttonColors(
+                containerColor = indigo_dye
+            ),
+
+            modifier = Modifier
+                .size(width = 159.dp, height = 42.dp),
+            shape = RoundedCornerShape(7.dp)
+        ) {
+            androidx.compose.material3.Text(
+                text = "Redefinir",
+                color = white_smoke,
+                fontSize = 20.sp,
+                //fontFamily = fontPoppins,
+                modifier = Modifier
+                    .align(alignment = Alignment.CenterVertically)
+            )
+        }
+    }
+}
+
+@Composable
+fun SenhaAlterada(navController: NavController) {
+    val splashScreenDuration = 1000L // 1000 ms = 1 segundos
+
+    LaunchedEffect(Unit) {
+        delay(splashScreenDuration)
+        navController.navigate("login") {
+            popUpTo("login") { inclusive = true }
+        }
+    }
+
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier
+            .fillMaxSize()
+            .paint(
+                painter = painterResource(
+                    R.drawable.background_color
+                ),
+                contentScale = ContentScale.Crop
+            )
+    ) {
+        Text(
+            "Senha alterada!",
+            fontSize = 28.sp,
+            fontWeight = FontWeight.SemiBold,
+            color = midnight_blue
+        )
+    }
+}
+
 @Preview
 @Composable
 fun RedefinirsenhaPreview() {
     DevEmpowerTheme {
         RedefinirSenha(rememberNavController())
+    }
+}
+
+@Preview
+@Composable
+fun MudarPreview() {
+    DevEmpowerTheme {
+        MudarSenha(rememberNavController())
+    }
+}
+
+@Preview
+@Composable
+private fun SenhaPreview() {
+    DevEmpowerTheme {
+        SenhaAlterada(rememberNavController())
     }
 }
 
