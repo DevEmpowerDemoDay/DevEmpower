ae1146ee4f5a65a75490af4bf37126adf1cc6add
fatekkl <mathtml.1105@gmail.com>
2024-11-16 11:03:00 -0300

    suspend menu

--- app/src/main/java/com/demoday/devempower/Home.kt
+++ app/src/main/java/com/demoday/devempower/Home.kt
@@ -1,85 +1,92 @@
 package com.demoday.devempower
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Home
-import androidx.compose.material.icons.filled.Person
-import androidx.compose.material3.Icon
-import androidx.compose.material3.NavigationBar
-import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import com.demoday.devempower.ui.theme.DevEmpowerTheme
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
 @Composable
-fun BottomNavigationMenu(navController: NavController) {
-    val uranium_blue = colorResource(id = R.color.uranium_blue)
-    val celadon = colorResource(id = R.color.celadon)
-    val davys_gray = colorResource(id = R.color.davys_gray)
-    val white_smoke = colorResource(id = R.color.white_smoke)
-    val indigo_dye = colorResource(id = R.color.indigo_dye)
+fun Home(navController: NavController) {
+    val systemUiController = rememberSystemUiController()
 
-    val selectedIndex = remember { mutableStateOf(0) }
+    systemUiController.setStatusBarColor(
+        indigo_dye
+    )
 
-        Box(modifier = Modifier.fillMaxSize()) {
-        NavigationBar(
-            containerColor = indigo_dye,
+    systemUiController.setNavigationBarColor(
+        uranium_blue,
+        darkIcons = true
+    )
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(uranium_blue)
+    ) {
+
+        Box(
             modifier = Modifier
-                .align(Alignment.BottomCenter)
-                .size(width = 357.dp, height = 176.dp)
-                .offset(y = (-66).dp)
-                .clip(RoundedCornerShape(38.dp))
+                .width(399.dp)
+                .height(263.dp)
+                .clip(RoundedCornerShape(bottomStart = 30.dp, bottomEnd = 30.dp))
+                .background(indigo_dye)
         ) {
-            Row {
-                NavigationBarItem(
-                    selected = selectedIndex.value == 0,
-                    onClick = { selectedIndex.value = 0 },
-                    icon = {
-                        Icon(
-                            imageVector = Icons.Default.Home,
-                            contentDescription = "Home",
-                            modifier = Modifier
-                                .size(width = 26.dp, height = 26.dp)
-                                .clip(CircleShape),
-                            tint = Color.Black,
-                        )
-                    },
-                    modifier = Modifier.weight(1f)
+
+            Column(
+                modifier = Modifier
+                    .padding(horizontal = 14.dp)
+                    .fillMaxSize(),
+                verticalArrangement = Arrangement.Center,
+            ) {
+                Image(
+                    painter = painterResource(R.drawable.logo),
+                    contentDescription = "Logo",
+                    modifier = Modifier.padding(vertical = 20.dp)
+                        .size(70.dp)
+                        .clip(RoundedCornerShape(50.dp))
+                        .background(white_smoke)
+                        .padding(horizontal = 14.dp)
                 )
 
-                NavigationBarItem(
-                    selected = selectedIndex.value == 0,
-                    onClick = { selectedIndex.value = 0 },
-                    icon = {
-                        Icon(
-                            imageVector = Icons.Default.Person,
-                            contentDescription = "Home",
-                            modifier = Modifier
-                                .size(width = 26.dp, height = 46.dp)
-                                .clip(CircleShape),
-                            tint = Color.Black,
-                        )
-                    },
-                )
+                Column(verticalArrangement = Arrangement.spacedBy((-15).dp)) {
+                    Text("Bem vindo,", color = white_smoke, fontSize = 15.sp)
+                    Text(
+                        " Desenvolvedor",
+                        color = white_smoke,
+                        fontSize = 40.sp,
+                        fontFamily = fontPoppins,
+                        fontWeight = FontWeight.SemiBold,
+                        modifier = Modifier.align(Alignment.CenterHorizontally),
+                        textAlign = TextAlign.Center
+                    )
+                }
+
+
             }
-
         }
+
     }
 }
 
@@ -87,6 +94,6 @@ fun BottomNavigationMenu(navController: NavController) {
 @Composable
 private fun BottomPreview() {
     DevEmpowerTheme {
-        BottomNavigationMenu(rememberNavController())
+        Home(rememberNavController())
     }
 }
